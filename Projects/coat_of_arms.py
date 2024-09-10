40ca1eb97d464c1b3edc8092adbea81376ef7e55
linkruby <nateshlarkin@seattleacademy.org>
2024-09-10 19:07:03 +0000

    project 1 submission

---
+++ Projects/coat_of_arms.py
@@ -0,0 +1,21 @@
+###############################################
+### SETUP ###
+import codesters
+from codesters import StageClass
+stage = StageClass()
+###############################################
+
+stage.set_background ("winter")
+
+s1 = codesters.Square ( 100, 100, 200, "red")
+s2 = codesters.Square ( 100, 100, 200, "yellow")
+s3 = codesters.Square ( 100, 100, 200, "green")
+s4 = codesters.Square ( 100, 100, 200, "blue")
+
+s5 = codesters.Sprite ("cameleon", 100,100)
+s5.set_size (0.1)
+s6 = codesters.Sprite ("tiger",100,-100)
+s6.set_size (0.2)
+s7 = codesters.Sprite ("fish",-100,100)
+s7.set_size(0.2)
+s8 = codesters.Sprite ("flower",-100,-100)
