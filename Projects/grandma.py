3cd4252219e248921d644bea40fd849d571a235c
linkruby <nateshlarkin@seattleacademy.org>
2024-09-18 17:52:18 +0000

    start_project 2

---
+++ Projects/grandma.py
@@ -0,0 +1,9 @@
+while True:
+    word = input ("What do you think grandma likes? ")
+
+    if "i" in word:
+        print(f"Grandma likes {word}!")
+    else:
+        print(f"Grandma doesn't like {word}")
+
+        print("")
---
+++ Projects/mystery.py
@@ -0,0 +1,25 @@
+# Intro 
+
+print("You wake up in a dark room with a chair in one corner and a bed in the other there appears to be no door in or out but more concerning you have no idea how you got here or why you are here")
+print("after the initial fear of where you are or how you got here you find a plate of food in the middle that was not there a minute ago it looks like bread and cheese if it is here there must be a way in and out!")
+
+# choice 1
+
+c1 = input("Do you want to explore the area by the [Food] or do you want to explore the [Chair and Bed]?")
+
+if "food" in c1:
+    print("you find a small hatch under the food you might be able to fit into it but you dont dont know whats under it ")
+
+    # choice 2
+
+    c2 = input("do you want to [go through] or [not]")
+    if "go through" in c2:
+        print("you are able to squeeze through the hatch and make it into a small dank room you see a door and walk through you then find yourself in a clearing in a forest the sun is shining and the birds are screaming wait the birds are screaming yes")
+        print("you start running away anything to escape what is happening you are so focused on running you dont notice the fact that the trees are watching you when you finally stop running you are no longer in a forest you are on the top of a mountain")
+        # choice 3
+        c3 = input("do you want to keep [running] or stay and [look around] ")
+        if "run" in c2:
+            print("you are stuck here forever")
+
+        else:
+            you are in a coma and this is the end of this particular option future me 
